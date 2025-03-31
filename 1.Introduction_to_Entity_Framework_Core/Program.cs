8e4f6a668f8862e55e55188e724ccfcf42f5fb3a
Weretik <vitaliythupin@gmail.com>
2025-03-31 22:24:07 +0300

    feat(ef-core): Initialize project 1 (Introduction to Entity Framework Core).

---
+++ 1.Introduction_to_Entity_Framework_Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.Introduction_to_Entity_Framework_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
