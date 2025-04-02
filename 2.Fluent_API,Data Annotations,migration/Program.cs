aaa053a8f9d46244ba057a132d43cd46fdd0b99e
Weretik <vitaliythupin@gmail.com>
2025-04-02 14:27:35 +0300

    feat(project): Remove old project and initialize project 2 – Fluent API, Data Annotations, migrations.

---
+++ 2.Fluent_API,Data Annotations,migration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2.Fluent_API_Data_Annotations_migration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
