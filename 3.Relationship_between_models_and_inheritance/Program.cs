2db1087d2226c2d8bc72e8b869a275a5a6535730
Weretik <vitaliythupin@gmail.com>
2025-04-03 13:27:37 +0300

    feat(project): Initialize project 3 (Relationship between models and inheritance).

---
+++ 3.Relationship_between_models_and_inheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3.Relationship_between_models_and_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
